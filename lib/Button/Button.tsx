f4713434f53bad26d34c76454816e9f78fbdb3e2
nptay1999 <nptay1999@gmail.com>
2024-06-18 20:59:05 +0700

    feat: export style css and dts files

--- lib/Button/Button.tsx
+++ lib/Button/Button.tsx
@@ -1,7 +1,8 @@
+import classes from "./Button.module.css";
 import { Button as AntButton, ButtonProps } from "antd";
 
-type TButtonProps = ButtonProps;
+export type TButtonProps = ButtonProps;
 
 export const Button = (props: TButtonProps) => {
-  return <AntButton {...props} />;
+  return <AntButton className={classes["button"]} {...props} />;
 };
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
+import dtsPlugin from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), dtsPlugin({ rollupTypes: true })],
   build: {
     /** Library entry and output setting */
     lib: {
